ea0ff7f07bc127d631624d79c4aa7282bb7640ca
Nirleka Arthamevira <nirlekaarthamevira@gmail.com>
2025-03-12 16:38:17 +0700

    upgraded to tailwind v4 and fixing some UI

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const navbar = document.getElementById("navbar"); 
+  const cardScroller = document.getElementById("card-scroller");
 
   window.addEventListener("scroll", function () {
 
@@ -7,11 +8,26 @@ document.addEventListener("DOMContentLoaded", function () {
       const navbarHeight = navbar.offsetHeight;
 
       if (scrollPosition > navbarHeight) {
-          navbar.classList.add("bg-accent_1_dark/65", "h-16", "backdrop-blur-md");
-          navbar.classList.remove("bg-gradient-to-b", "from-accent_1/15", "from-30%", "h-32")
+          navbar.classList.add("bg-accent1dark/65", "h-16", "backdrop-blur-md");
+          navbar.classList.remove("bg-linear-to-b", "from-accent1/15", "from-30%", "h-32")
         } else {
-          navbar.classList.remove("bg-accent_1_dark/65", "h-16", "backdrop-blur-md");
-          navbar.classList.add("bg-gradient-to-b", "from-accent_1/15", "from-30%", "h-32")
+          navbar.classList.remove("bg-accent1dark/65", "h-16", "backdrop-blur-md");
+          navbar.classList.add("bg-linear-to-b", "from-accent1/15", "from-30%", "h-32")
       }
   });
+
+  // Menambahkan event listener untuk event resize
+  window.addEventListener('resize', () => {
+    const width = window.innerWidth;
+
+    if (width >= 1024)
+    {
+      cardScroller.classList.remove("scrollbar-hidden");
+      cardScroller.classList.add("custom-scrollbar");
+    } else{
+      cardScroller.classList.add("scrollbar-hidden");
+      cardScroller.classList.remove("custom-scrollbar");
+    }
+  });
+
 });
