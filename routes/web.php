77fdbd23b4111019e7c1fa3f2b1dc8faaf8decad
nirlekaartha <nirlekaarthamevira@gmail.com>
2025-07-08 21:20:37 +0700

    feat: added auth middleware group

--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,6 @@ Route::get('/posts', function () {
     return view('posts');
 });
 
-Route::get('/post-form', function () {
-    return view('post-form');
-});
-
 Route::get('/post/{slug}', function ($slug) {
 
     $post = Post::where('slug', $slug)->firstOrFail();
@@ -34,36 +30,31 @@ Route::get('/post/{slug}', function ($slug) {
     return view('post', ["post" => $post]);
 });
 
-// Route::get('/login', function () {
-//     return view('login');
-// });
 
-// Route::get('/sign-up', function () {
-//     return view('sign-up');
-// });
+Route::middleware('auth')->group(function () {
+    
+    Route::get('/post-form', function () {
+    return view('post-form');
+    });
 
-// Route::get('/recovery', function () {
-//     return view('recovery');
-// });
-
-Route::get('/settings', function () {
+    Route::get('/settings', function () {
     return redirect("/settings/account");
+    });
+
+    Route::get('/settings/account', function () {
+        return view('settings.account');
+    });
+
+    Route::get('/settings/saved', function () {
+        return view('settings.saved');
+    });
+
+    Route::get('/settings/security', function () {
+        return view('settings.security');
+    });
+
+    Route::get('/settings/your-stories', function () {
+        return view('settings.your-stories');
+    });
+
 });
-
-Route::get('/settings/account', function () {
-    return view('settings.account');
-});
-
-Route::get('/settings/saved', function () {
-    return view('settings.saved');
-});
-
-Route::get('/settings/security', function () {
-    return view('settings.security');
-});
-
-Route::get('/settings/your-stories', function () {
-    return view('settings.your-stories');
-});
-
-
